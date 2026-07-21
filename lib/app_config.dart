633f02144e9a955efc5a799d7d9bce7ecbb7d4de
foyselmollik3120-crypto <foyselmollik3120@gmail.com>
2026-07-21 22:00:33 +0600

    Update baseUrl to new backend API endpoint

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   // আপনার ব্যাকএন্ড URL এখানে দিন
-  static const String baseUrl = "https://your-backend-api.com/api";
+  static const String baseUrl = "https://fmb-anh3.onrender.com";
 }
 
